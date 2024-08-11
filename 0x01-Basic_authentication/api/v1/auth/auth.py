d2d25516fbaadb28a96cb9c7558b0ae417715edc
fares002 <fares@example.com>
2024-08-11 22:59:47 +0000

    task2

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,27 +7,50 @@ import fnmatch
 
 
 class Auth:
-    """Authentication class.
-    """
+    """Authintacation """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required.
+        """
+        Checks if authentication is required for a
+        given path based on a list of excluded paths.
+
+        Parameters:
+        - path (str): The path to check.
+        - excluded_paths (List[str]):
+        A list of paths that do not require authentication.
+        Paths can include a wildcard '*' at the end.
+
+        Returns:
+        - bool: True if the path is None,
+        if excluded_paths is None or empty, or if the path is not
+        in excluded_paths. Returns False if the path matches any
+        of the excluded paths or
+        the pattern specified by the wildcard '*'.
         """
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        normalize_path = path.rstrip('/')
+
+        # Normalize the input path by stripping trailing slashes
+        normalized_path = path.rstrip('/')
 
         for excluded_path in excluded_paths:
+            # Normalize the excluded_path and handle wildcard '*'
             normalized_excluded_path = excluded_path.rstrip('/')
 
-            if normalized_excluded_path.endswith("*"):
-                if normalize_path.startswith(normalized_excluded_path[0:-1]):
+            if normalized_excluded_path.endswith('*'):
+                # Remove the wildcard character and normalize the prefix
+                prefix = normalized_excluded_path[:-1]
+                # Check if the normalized path starts with the prefix
+                if (normalized_path.startswith(prefix) and
+                        len(normalized_path) >= len(prefix)):
+                    return False
+            else:
+                # Normal case: exact match
+                if normalized_path == normalized_excluded_path:
                     return False
 
-            else:
-                if normalize_path == normalized_excluded_path:
-                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get authorization header.
