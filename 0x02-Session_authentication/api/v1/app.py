7ea7ba4fef77790a57ab34c8964ea3fb2b457a7e
fares002 <faresosama002@gmail.com>
2024-08-14 22:12:19 +0300

    1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,18 +30,17 @@ def before_request():
     Handle request filtering before the actual request processing.
     """
     if auth is None:
-        return
-    request.current_user = auth.current_user(request)
+        pass
+    else:
+        request.current_user = auth.current_user(request)
 
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-        ]
-    
-
-    if request.path not in excluded_paths:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+            ]
+        
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
                 abort(401)  # Unauthorized
