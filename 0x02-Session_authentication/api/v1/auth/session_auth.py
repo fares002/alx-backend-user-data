0f392589d1dfeae3a8fa3bba70cae4a8ad02d81b
fares002 <faresosama002@gmail.com>
2024-08-14 21:36:51 +0300

    1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
     
     def create_session(self, user_id: str = None) -> str:
+        """assing session id to user_id"""
         if user_id is None or not isinstance(user_id, str):
             return None
         
