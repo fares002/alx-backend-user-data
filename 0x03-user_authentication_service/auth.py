af6ccffa5938bf671b1b240ee278567e7e36c9fc
fares002 <faresosama002@gmail.com>
2024-08-16 02:05:43 +0300

    task1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,6 +9,30 @@ from uuid import uuid4
 from sqlalchemy.orm.exc import NoResultFound
 
 
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers new user
+            Args:
+                - email: user's email
+                - password: user's password
+            Return:
+                - User instance created
+        """
+        db = self._db
+        try:
+            user = db.find_user_by(email=email)
+        except NoResultFound:
+            user = db.add_user(email, _hash_password(password))
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
+
 def _hash_password(password: str) -> bytes:
     """ Creates password hash
         Args:
