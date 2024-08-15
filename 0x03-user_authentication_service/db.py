4c787e30a0217c898e71e221312d652dd8705a7f
fares002 <faresosama002@gmail.com>
2024-08-15 22:05:33 +0300

    task1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,13 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine, tuple_
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-
-from user import Base
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
 
 
 class DB:
@@ -18,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,23 +31,29 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Create new user instance 
-        and save it to the database"""
+        """ Creates new User instance and
+            saves them to the database.
+            Args:
+                - email
+                - hashed_password
+            Return:
+                - new User object
+        """
         session = self._session
         try:
-            new_user = User(email=email, hashed_password = hashed_password)
+            new_user = User(email=email, hashed_password=hashed_password)
             session.add(new_user)
             session.commit()
         except Exception:
             session.rollback()
             new_user = None
         return new_user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """ Find user by a given attribute
             Args:
                 - Dictionary of attributes to use as search
-                parameters
+                  parameters
             Return:
                 - User object
         """
@@ -67,3 +71,18 @@ class DB:
         if not user:
             raise NoResultFound()
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Searches for user instance using given id parameter
+            Args:
+                - user_id: user's id
+            Return:
+                - User instance found
+        """
+        user = self.find_user_by(id=user_id)
+        session = self._session
+        for attr, val in kwargs.items():
+            if not hasattr(User, attr):
+                raise ValueError
+            setattr(user, attr, val)
+        session.commit()
