dead140fcdfd8def6d4a1889e77919946d996aba
fares002 <faresosama002@gmail.com>
2024-08-15 21:26:19 +0300

    task1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,7 +11,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     hased_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
