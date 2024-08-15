5dacb0c560050deff88cd3475815dd0a62dc5113
fares002 <faresosama002@gmail.com>
2024-08-16 02:47:28 +0300

    task1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -27,4 +27,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-    
