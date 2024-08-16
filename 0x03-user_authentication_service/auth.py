06736a9fea007d3bf83e1754a3b4114619c2fc5d
fares002 <faresosama002@gmail.com>
2024-08-16 03:49:04 +0300

    task1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,7 +56,7 @@ class Auth:
         session_id = _generate_uuid()
         db.update_user(user.id, session_id=session_id)
         return session_id
-    
+
     def get_user_from_session_id(self, session_id: str) -> User:
         """Get user by session id"""
         if session_id is None:
